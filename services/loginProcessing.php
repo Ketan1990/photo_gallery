f885165e85edf56217bd72c0356cd1670a696ac3
ketan <ketan.jain@live.com>
2015-05-22 21:21:06 +0530

    change path

--- services/loginProcessing.php
+++ services/loginProcessing.php
@@ -15,12 +15,12 @@ $postData = file_get_contents("php://input");
 $request = json_decode($postData);
 $name = $request->user;
 $pass = $request->pass;
+
 $user=new \Interactor\UsersDAO(new \Interactor\MySqlQueryEngine());
 $found_user = $user->authenticate($name,$pass);
 if (isset($found_user->id)) {
     $session->login($found_user);
     echo $name;
-
 } else {
     header("HTTP/1.0 204 Not content");
 }
